c8d0821244b1d4d49ea702ea03e0b0530eb0ea82
ssalustri19 <ssalustri19@amherst.edu>
2018-12-03 18:31:04 -0500

    more shiny app code

--- app.R
+++ app.R
@@ -4,6 +4,8 @@ library(DT)
 library(twitteR)
 library(dplyr)
 library(lubridate)
+library(tidytext)
+library(tm)
 
 politicians<-read_csv('https://raw.githubusercontent.com/ssalustri19/final-project-idil-sammy/master/politicians.csv')
 
@@ -16,29 +18,64 @@ ui<- fluidPage(
       selectInput(inputId = "politician",
                   label="Choose a politician",
                   choices= politicians$twitter_handle,
-                  selectize = TRUE)
+                  selectize = TRUE),
+      numericInput(inputId = "numtweets",
+                   label="Select number of most recent tweets to analyze. Max=3200",
+                   value=100,
+                   min=1,
+                   max=3199,
+                   step=100),
+      actionButton(inputId="goButton",
+                   label="Run Analysis")
+  
     ),
     mainPanel(
-      DT::dataTableOutput(outputId = "tweetstable")
+      tabsetPanel(tabPanel("Raw Tweets Data Frame", DT::dataTableOutput(outputId = "tweetstable")),
+                  tabPanel("Word Frequency Table", DT::dataTableOutput(outputId = "freqtable")),
+                  tabPanel("Word Cloud", plotOutput(outputId="cloud"))
+      )
+              
     )
   )
-  
-  
-  
-  
 )
 
+  
+  
+  
+  
+
+
 server<- function(input,output){
+
+  
+  tweets_from_selected_politician<-eventReactive(input$goButton,
+                                                 {userTimeline(input$politician, n = input$numtweets, includeRts = FALSE)%>%
+                                                  twListToDF() %>% 
+                                                  mutate(date=lubridate::date(created)) %>% 
+                                                  select(text, date)
+                                                  })
+  
+  output$freqtable <- DT::renderDataTable({
+    raw<-tm::termFreq(tweets_from_selected_politician()$text, control = list(removePunctuation = TRUE, tolower = TRUE, stopwords = TRUE)) 
+    df <- as.data.frame(melt(as.matrix(raw), varnames = c("word", "some"))) %>% select(-some)
+    df$word <- as.character(df$word)
+    df2<- df %>% arrange(desc(value)) 
+    DT::datatable(data = df2, 
+                options = list(pageLength = 10), 
+                rownames = FALSE)
+  })
+    
+
+
+  
   output$tweetstable<-DT::renderDataTable({
-    req(input$politician)
-    tweets_from_selected_politician<-userTimeline(input$politician, n=100, includeRts = FALSE)%>%
-    twListToDF() %>% select(text, created, screenName)
-    DT::datatable(data = tweets_from_selected_politician, 
+    DT::datatable(data = tweets_from_selected_politician(), 
                   options = list(pageLength = 10), 
                   rownames = FALSE)
   })
-  
-  
 }
+  
+  
+
 
 shinyApp(ui=ui, server=server)
